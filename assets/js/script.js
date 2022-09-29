0a6b643dfaca5d0be9f2e7df6e2c8dc2109c0e28
danio86 <d.harder@gmx.at>
2022-09-29 15:30:38 +0000

    add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1,num2);
     } else if(gameType === 'subtract'){
         displaySubtractQuestion(num1,num2);
+    } else if(gameType === 'division'){
+        displayDivisionQuestion(num1,num2);
     } else {
             alert(`Unkown game type: ${gameType}`);
             throw `Unkown game type: ${gameType}. Aborting!`;
@@ -91,6 +93,8 @@ function calculateCorrectAnswer() {
         return[operand1 * operand2, 'multiply'];
     } else if(operator === '-') {
         return[operand1 - operand2, 'subtract'];
+    } else if(operator === '/') {
+        return[operand1 / operand2, 'division'];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -134,4 +138,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2; 
+    //if op1 is > than op2: give op1 else give op2
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '/';
 }

c1bfceafc300d3686bf6a63c51d19cc2693d603a
danio86 <d.harder@gmx.at>
2022-09-29 08:45:39 +0000

    add addition function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame('addition');
 });
 
 
@@ -21,10 +22,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * Dot string beschreibt function - Dadurch wird die Funktion beschrieben wenn sie woanders gecalled wird:
  * Mein Loop of the game > Called when the game loads and when the users answer is proceed
  */
-function runGame() {
+function runGame(gameType) {
     // creates two random numbers between 1-25 
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
+
+    if(gameType === 'addition'){
+        displayAdditionQuestion(num1,num2);
+        else{
+            alert(`Unkown game type: ${gameType}`)
+            throw `Unkown game type: ${gameType}. Aborting!`
+            // breaks the code and prints this message
+        }
+    }
 
 }
 
@@ -44,8 +54,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
